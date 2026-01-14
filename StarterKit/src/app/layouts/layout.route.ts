11d80edde07d4b246169cd1e53973d8e03e45150
Ajagbe Uthman Olawale <uthman4u2nv@yahoo.com>
2026-01-14 09:02:24 +0100

    Delete StarterKit directory

--- StarterKit/src/app/layouts/layout.route.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from '../pages/dashboard/dashboard/dashboard.component';
-
-export const PAGE_ROUTES: Routes = [
-  { path: '', component: DashboardComponent },
-]
